6a16e815decbddd2fcea8c9f1c60cb8792b7fca2
Rubens Gomes <rubens.gomes@aa.com>
2021-03-04 04:06:29 +0000

    fixes

--- static/js/contact-form.js
+++ static/js/contact-form.js
@@ -1,38 +1,27 @@
-/**
- * @file Manages interactions with the contact-form.
- * 
- * This script relies on:
- * <ul>
- * <li>{@link https://jquery.com/ jQuery}</li>
- * <li>{@link https://jqueryvalidation.org/ jQuery Validation Plugin} </li>
- * <li>{@link https://www.google.com/recaptcha/intro/v3.html Google reCAPTCHA}</li>
- * </ul>
- * 
- * @author Rubens Gomes <rubens.s.gomes@gmail.com>
- */
+// JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
- // jQuery - ensure JQuery is loaded
-if (window.jQuery === 'undefined') {
-  console.log('jQuery library is *not* loaded.');
-  throw new Error('The jQuery JavaScript library was not loaded!');
-}
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-
-// JQuery Validate Plugin - validate contact form
-$().ready(function() {
-  // validate form
-  var validator = $('#contact-form').validate();
-
-  // clear form
-  $('#reset-btn').click(function() {
-    validator.resetForm();
-  });
-});
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
 
 // Google reCaptcha V2 - validate captcha is checked
-var onloadCallback = function () {
+var onloadCallback = function() {
   grecaptcha.render('recaptcha-contact', {
     sitekey: '6LcIV7IZAAAAADs-VOVIP2Grqc6AbKCFQF94xIPn',
   });
@@ -42,7 +31,7 @@ var onloadCallback = function () {
 var form = document.getElementById('contact-form');
 form.addEventListener(
   'submit',
-  function (event) {
+  function(event) {
     if (grecaptcha.getResponse() === '') {
       event.preventDefault();
       // display bootstrap modal dialog if captcha not checked
